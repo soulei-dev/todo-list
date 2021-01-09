75f9fde1093271a9b9bd1181f6c8b484de17f468
Souleimane Kouygou <soulei.dev@gmail.com>
2021-01-09 11:38:21 +0100

    update gitignore file

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 use: ["babel-loader"]
             },
             {
-                test: /\.css$/,
+                test: /\.css$/i,
                 use: ["style-loader", "css-loader"]
             }
         ]
